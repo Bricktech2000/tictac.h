8adc997ffbc099b0015125ed3606f83d8a89ed8f
Emilien Breton <bricktech2000@gmail.com>
2024-09-19 20:34:25 -0400

    exclude calls to `fprintf` from total CPU time

--- tictac.h
+++ tictac.h
@@ -6,13 +6,14 @@
 
 #define TIC(label)                                                             \
   ; /* for labels */                                                           \
-  clock_t _start_##label = clock(), _elap_##label;                             \
+  clock_t _start_##label = clock();                                            \
   static clock_t _total_##label = 0
 #define TAC(label)                                                             \
-  _elap_##label = clock() - _start_##label, _start_##label += _elap_##label,   \
+  _start_##label = clock() - _start_##label,                                   \
   fprintf(TICTAC_FILEP, "%.*f\t%.*f\t%s\n", TICTAC_PREC,                       \
-          _elap_##label * 1.0 / CLOCKS_PER_SEC, TICTAC_PREC,                   \
-          (_total_##label += _elap_##label) * 1.0 / CLOCKS_PER_SEC, #label)
+          _start_##label * 1.0 / CLOCKS_PER_SEC, TICTAC_PREC,                  \
+          (_total_##label += _start_##label) * 1.0 / CLOCKS_PER_SEC, #label),  \
+  _start_##label = clock() /* exclude call to `fprintf` */
 #define TICTAC(label)                                                          \
   TIC(label);                                                                  \
   for (int _tictac = 1; _tictac; TAC(label), _tictac = 0)
