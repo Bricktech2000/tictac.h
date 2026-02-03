72c1d979dde77e4ffa55c8766b0f6d0657c81814
Emilien Breton <bricktech2000@gmail.com>
2026-02-03 11:40:25 -0500

    eliminated dependence on floats

--- tictac.h
+++ tictac.h
@@ -1,19 +1,25 @@
 #include <stdio.h>
 #include <time.h>
 
-#define TICTAC_PREC 6
+#define TICTAC_SCALE 1000000
+#define TICTAC_FORMAT "%8.6lld\t"
 #define TICTAC_STREAM stderr
+
+// #define TICTAC_SCALE 1.0
+// #define TICTAC_FORMAT "%f,"
+// #define TICTAC_STREAM stdout
 
 #define TIC(LABEL)                                                             \
   ; /* for labels */                                                           \
   clock_t _start_##LABEL = clock();                                            \
   static clock_t _total_##LABEL = 0
 #define TAC(LABEL)                                                             \
-  _start_##LABEL = clock() - _start_##LABEL,                                   \
-  fprintf(TICTAC_STREAM, "%.*f\t%.*f\t%s\n", TICTAC_PREC,                      \
-          _start_##LABEL * 1.0 / CLOCKS_PER_SEC, TICTAC_PREC,                  \
-          (_total_##LABEL += _start_##LABEL) * 1.0 / CLOCKS_PER_SEC, #LABEL),  \
-  _start_##LABEL = clock() /* exclude call to `fprintf` */
+  _total_##LABEL += (_start_##LABEL = clock() - _start_##LABEL),               \
+      fprintf(TICTAC_STREAM, TICTAC_FORMAT TICTAC_FORMAT "%s\n",               \
+              (long long)_start_##LABEL * TICTAC_SCALE / CLOCKS_PER_SEC,       \
+              (long long)_total_##LABEL * TICTAC_SCALE / CLOCKS_PER_SEC,       \
+              #LABEL),                                                         \
+      _start_##LABEL = clock() /* exclude call to `fprintf` */
 #define TICTAC(LABEL)                                                          \
   /* ISO/IEC 9899:TC3, $7.23.1 requires `clock_t` to be an arithmetic type */  \
   for (clock_t _start_##LABEL = clock(), _tictac = 1; _tictac; _tictac = 0)    \
